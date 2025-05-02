b418f642c9a2ef4e215b66da7e24fececd8b5b5d
yaroluchko <yaroslavluchko6@gmail.com>
2025-05-02 01:02:34 -0400

    [feat][web-export] move patching functions to seperate class

--- src/timing/TimingStrategyManager.js
+++ src/timing/TimingStrategyManager.js
@@ -2,6 +2,7 @@ import * as Tone from 'tone';
 import { timingObjectManager } from './TimingObjectManager.js';
 import midiClockManager from '../midi/MidiClockManager.js';
 import toneJsSync from './ToneJsSync.js';
+import TonePatcher from './TonePatcher.js';
 
 /**
  * TimingStrategyManager - Centralized manager for different timing strategies
@@ -25,11 +26,8 @@ class TimingStrategyManager {
         // Current active strategy
         this.activeStrategy = this.STRATEGIES.TONE_JS;
 
-        // Patch Tone.Loop to automatically register with our manager
-        this.patchToneLoop();
-
-        // Patch Tone.Transport.start to prevent it from working with MIDI clock
-        this.patchToneTransport();
+        // Create the tone patcher to handle patching Tone.js components
+        this.tonePatcher = new TonePatcher(this);
     }
 
     /**
@@ -210,80 +208,6 @@ class TimingStrategyManager {
                 Tone.getTransport().bpm.value = bpm;
                 break;
         }
-    }
-
-    /**
-     * Register a Tone.Loop with toneJsSync
-     * @param {Tone.Loop} loop - The loop to register
-     * @param {string|number} [interval] - Optional interval override
-     * @returns {string|null} - ID of the registered loop
-     */
-    registerLoop(loop, interval = null) {
-        // Always register with toneJsSync to ensure loops are available for MIDI clock
-        return toneJsSync.registerLoop(loop, interval);
-    }
-
-    /**
-     * Unregister a loop from toneJsSync
-     * @param {string|Tone.Loop} loopOrId - Loop instance or ID to unregister
-     */
-    unregisterLoop(loopOrId) {
-        if (typeof loopOrId === 'string') {
-            toneJsSync.unregisterLoop(loopOrId);
-        } else {
-            // Find the loop ID by loop instance in toneJsSync's registry
-            for (const [id, loopData] of toneJsSync.registeredLoops.entries()) {
-                if (loopData.loop === loopOrId) {
-                    toneJsSync.unregisterLoop(id);
-                    break;
-                }
-            }
-        }
-    }
-
-
-    /**
-     * Patch Tone.Loop to automatically register with our manager and track lifecycle
-     * @private
-     */
-    patchToneLoop() {
-        const originalStart = Tone.Loop.prototype.start;
-        const originalDispose = Tone.Loop.prototype.dispose;
-        const manager = this;
-
-        // Override the start method to automatically register loops
-        Tone.Loop.prototype.start = function (time) {
-            manager.registerLoop(this);
-            return originalStart.call(this, time);
-        };
-
-        // Override the dispose method to unregister loops when they're disposed
-        Tone.Loop.prototype.dispose = function () {
-            // Unregister the loop before disposing it
-            manager.unregisterLoop(this);
-            return originalDispose.call(this);
-        };
-    }
-
-    /**
-     * Patch Tone.Transport to prevent start() from working when MIDI clock is active
-     * @private
-     */
-    patchToneTransport() {
-        const originalStart = Tone.getTransport().start;
-        const manager = this;
-
-        // Override the start method to prevent it from working with MIDI clock
-        Tone.getTransport().start = function (time, offset) {
-            // If MIDI clock strategy is active, prevent direct transport start
-            if (manager.activeStrategy === manager.STRATEGIES.MIDI_CLOCK) {
-                console.warn('Cannot start Tone.Transport directly when MIDI clock strategy is active');
-                return this;
-            }
-
-            // Otherwise, use the original start method
-            return originalStart.call(this, time, offset);
-        };
     }
 }
 
