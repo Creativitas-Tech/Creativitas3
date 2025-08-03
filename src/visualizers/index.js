ed5a774c536b5ac95181bdeaad7c364906f854c3
hsavoldy <hsavoldy@mit.edu>
2025-08-03 19:19:55 +0800

    Added multirowseqgui to visualizers/index.js

--- src/visualizers/index.js
+++ src/visualizers/index.js
@@ -1,4 +1,5 @@
 export { Oscilloscope } from './Oscilloscope.js';
 export { Spectroscope } from './Spectroscope.js';
 export { Spectrogram } from './Spectrogram.js';
+export { MultiRowSeqGui } from './MultiRowSeq.js';
 export { PlotTransferFunction } from './PlotTransferFunction.js';
