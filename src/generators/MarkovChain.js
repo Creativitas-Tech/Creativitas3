2b9503d69f75182672ba5c4bbaa634fda50d5890
ianhattwick <ian@ianhattwick.com>
2025-11-19 11:33:28 -0500

    Update MarkovChain.js

--- src/generators/MarkovChain.js
+++ src/generators/MarkovChain.js
@@ -7,9 +7,9 @@ export class MarkovChain {
         this.startGram = []
         this.sourceSequence = []
         this.currentState = []
-        this.currentStateString = ''
     }
     //create a transition matrix from an array
+    train(arr){ this.createMarkovTable(arr)}
     createMarkovTable(arr) {
         this.sourceSequence = arr
         this.matrix = {}
@@ -19,11 +19,28 @@ export class MarkovChain {
           this.matrix[this.gram].push(arr[i+this.order])
         }
     }
-    train(arr){ this.createMarkovTable(arr)}
-    //get the next state
-    getNextState() {
+
+    //set the state to start generating output
+    //by specifying the number of one of the states
+    //the actual state selected is hard to predict,but repeatable
+    //returns the last entry of the state selected
+    start(num=0){
+        if(this.matrix[Object.keys(this.matrix)[num]]) this.currentState =Object.keys(this.matrix)[num]
+        else this.currentState = this.matrix[Object.keys(this.matrix)[0]]
+        this.currentState = this.stringToGram(this.currentState)
+        return this.currentState[this.currentState.length-1]
+    }
+    //get the next state based on the currentState
+    //optional argument to set the currentState
+    get(prev=null){
+        return this.getNextState(prev)
+    }
+    getNextState(current = null) {
+        if( current !== null ){
+            if(this.matrix[Object.keys(this.matrix)[current]]) this.currentState = this.stringToGram(Object.keys(this.matrix)[current])
+        }
         if (this.currentState === undefined || this.currentState.length<1) {
-          this.currentState = this.matrix[Object.keys(this.matrix)[0] ]
+          this.currentState = this.stringToGram(Object.keys(this.matrix)[0] )
         }
         this.possibleNextStates = this.matrix[this.gramToString(this.currentState)];
         if(this.possibleNextStates == undefined) return this.currentState[this.currentState.length-1]
@@ -51,26 +68,27 @@ export class MarkovChain {
         }
         return this.generatedSequence;
     }
-  gramToString(gram) { return gram == undefined ? 'undefined' : gram.join(' ');}
-  stringToGram(str) { return str == undefined ? 'undefined' : str .trim().split(' ').map(pair => pair.split(','));}
-  //download the matrix as a json file
-  download() {
-    this.name = window.prompt("File name (without extension):", "markov");
-    if (!this.name) return;
-  
-    this.data = JSON.stringify(this.matrix, null, 0);
-    this.blob = new Blob([this.data], { type: "application/json" });
-    this.url = URL.createObjectURL(this.blob);
-  
-    this.a = document.createElement("a");
-    this.a.href = this.url;
-    this.a.download = this.name + ".json";
-    this.a.style.display = "none";
-  
-    document.body.appendChild(this.a);
-    this.a.click();
-    document.body.removeChild(this.a);
-  
-    URL.revokeObjectURL(this.url);
-  }
+    //helper functions to convert the state from a string to an array and back
+    gramToString(gram) { return gram == undefined ? 'undefined' : gram.join(' ');}
+    stringToGram(str) { return str == undefined ? 'undefined' : str .trim().split(' ').map(pair => pair.split(','));}
+    //download the matrix as a json file
+      download() {
+        this.name = window.prompt("File name (without extension):", "markov");
+        if (!this.name) return;
+      
+        this.data = JSON.stringify(this.matrix, null, 0);
+        this.blob = new Blob([this.data], { type: "application/json" });
+        this.url = URL.createObjectURL(this.blob);
+      
+        this.a = document.createElement("a");
+        this.a.href = this.url;
+        this.a.download = this.name + ".json";
+        this.a.style.display = "none";
+      
+        document.body.appendChild(this.a);
+        this.a.click();
+        document.body.removeChild(this.a);
+      
+        URL.revokeObjectURL(this.url);
+      }
 }
